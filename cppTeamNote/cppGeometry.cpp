2f05f96767b50c44f53140337adeb1b6ca9f613c
지호 <mmm7407@yonsei.ac.kr>
2024-06-04 11:09:16 +0900

    0604

--- cppTeamNote/cppGeometry.cpp
+++ cppTeamNote/cppGeometry.cpp
@@ -1,23 +1,11 @@
-#include <bits/stdc++.h>
-#define ll long long
-#define lll __int128
-#define ld long double
-#define rep(i,l,r)for(ll i=(l);i<(r);i++)
-using namespace std;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-
-
-void fastio(){
-    cin.tie(0);
-    cout.tie(0);
-    ios_base::sync_with_stdio(false);
-}
+// Convex Hull
+// 볼록 껍질
 
 int N;
 vector<pll> points;
 vector<pll> hull;
 
+// AB x AC
 ll ccw(pll a, pll b, pll c){
     pll ab = {b.first - a.first, b.second - a.second};
     pll ac = {c.first - a.first, c.second - a.second};
@@ -27,6 +15,7 @@ ll ccw(pll a, pll b, pll c){
     return 0;
 }
 
+// AB x CD
 ll cccw(pll a, pll b, pll c, pll d){
     pll ab = {b.first - a.first, b.second - a.second};
     pll cd = {d.first - c.first, d.second - c.second};
@@ -36,19 +25,23 @@ ll cccw(pll a, pll b, pll c, pll d){
     return 0;
 }
 
+// AB^2
 ll dist(pll a, pll b){
     return (a.first-b.first)*(a.first-b.first) + (a.second-b.second)*(a.second-b.second);
 }
 
+// sort by x, y
 bool cmp(pll a, pll b){
     if(a.first == b.first) return a.second < b.second;
     return a.first < b.first;
 }
 
+// sort by angle
 bool cmp2(pll a, pll b){
     return ccw(points[0], a, b)>0 || ccw(points[0], a, b)==0 && a < b;
 }
 
+// make convex hull
 void makehull(){
     sort(points.begin(), points.end(), cmp);
     sort(points.begin()+1, points.end(), cmp2);
@@ -70,51 +63,18 @@ void makehull(){
     return;
 }
 
-void solve(){
-    cin >> N;
-    rep(i, 0, N){
-        ll x, y; cin >> x >> y;
-        points.push_back({x, y});
-    }
-    if(N == 2) {
-        for(auto &p : points) cout << p.first << ' ' << p.second << ' ';
-        cout << '\n';
-        return;
-    }
-    makehull();
+// rotating calipers
+ll rotating_calipers(){
     ll mx = 0, sz = hull.size();
-    pair<pll, pll> ans = {hull[0], hull[1]};
     ll bar2 = 1;
     rep(bar1, 0, sz){
         while(1){
             if(cccw(hull[bar1], hull[(bar1+1)%sz], hull[bar2], hull[(bar2+1)%sz]) <= 0) break;
-            ll nd = dist(hull[bar1], hull[bar2]);
-            if(nd > mx){
-                mx = nd;
-                ans = {hull[bar1], hull[bar2]};
-            }
+            mx = max(mx, dist(hull[bar1], hull[bar2]));
             bar2 = (bar2+1)%sz;            
         }
         ll nd = dist(hull[bar1], hull[bar2]);
-        if(nd > mx){
-            mx = nd;
-            ans = {hull[bar1], hull[bar2]};
-        }
+        mx = max(mx, nd);
     }
-
-    cout << ans.first.first << ' ' << ans.first.second << ' ' << ans.second.first << ' ' << ans.second.second << '\n';
-    
-    points.clear();
-    hull.clear();
-    return;
-}
-
-int main(){
-    fastio();
-    int tc = 1;
-    cin >> tc;
-    while(tc--){
-        solve();
-    }
-    return 0;
+    return mx;
 }
